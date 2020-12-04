c356f7cb3a11a174847b2cad4ba5ed2c5a97b3d7
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-12-04 20:05:46 +0000

    Update Testing-JS.js

--- Testing-JS.js
+++ Testing-JS.js
@@ -1,24 +1,62 @@
-function switchOfStuff(val) {
-    var answer = "";
-    // Only change code below this line
-      switch(val){
-          case "a":
-              answer = "apple";
-              break;
-          case "b":
-              answer = "bird";
-              break;
-          case "c":
-              answer = "cat";
-              break;
-          default:
-              answer = "stuff";
-              break;
-      }
+// function switchOfStuff(val) {
+//     var answer = "";
+//     // Only change code below this line
+//       switch(val){
+//           case "a":
+//               answer = "apple";
+//               break;
+//           case "b":
+//               answer = "bird";
+//               break;
+//           case "c":
+//               answer = "cat";
+//               break;
+//           default:
+//               answer = "stuff";
+//               break;
+//       }
     
-    // Only change code above this line
-    return answer;
-  }
+//     // Only change code above this line
+//     return answer;
+//   }
   
-  console.log(switchOfStuff("a"));
+//   console.log(switchOfStuff("a"));
   
+
+
+
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+        count + 1;
+        break;
+    case 7:
+    case 8:
+    case 9:
+        count;
+        break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+        count -1;
+        break;
+}
+if (count >=1){
+    return count + " Bet";
+} else {
+    return count + " Hold"
+}
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
