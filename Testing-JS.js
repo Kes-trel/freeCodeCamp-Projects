91361ab8de514ea51d91006567cc3107cbc312d6
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-12-05 19:33:43 +0000

    Update Testing-JS.js

--- Testing-JS.js
+++ Testing-JS.js
@@ -62,14 +62,56 @@
 
 
 
-function checkObj(obj, checkProp) {
-    // Only change code below this line
-    if(obj.hasOwnProperty(checkProp)==true) {
-      return obj[checkProp];
-    } else {
-      return "Not Found";
-    }
-    // Only change code above this line
-  }
+// function checkObj(obj, checkProp) {
+//     // Only change code below this line
+//     if(obj.hasOwnProperty(checkProp)==true) {
+//       return obj[checkProp];
+//     } else {
+//       return "Not Found";
+//     }
+//     // Only change code above this line
+//   }
 
-  console.log(checkObj({gift: "pony", pet: "kitten", bed: "sleigh"}, "gift"))
+//   console.log(checkObj({gift: "pony", pet: "kitten", bed: "sleigh"}, "gift"))
+
+
+// Setup
+var collection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(object, id, prop, value) {
+  return object;
+}
+
+console.log(updateRecords(collection, 5439, 'artist', 'ABBA'));
+
+/*
+Your function must always return the entire object.
+
+If prop isn't tracks and value isn't an empty string, update or set that album's prop to value.
+
+If prop is tracks but the album doesn't have a tracks property, create an empty array and add value to it.
+
+If prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array.
+
+If value is an empty string, delete the given prop property from the album.
+Note: A copy of the collection object is used for the tests.
+*/
