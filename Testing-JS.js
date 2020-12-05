967978878bd86530ff7ce9f589a80662522db490
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-12-05 18:42:34 +0000

    update testing docs

--- Testing-JS.js
+++ Testing-JS.js
@@ -25,38 +25,51 @@
 
 
 
-var count = 0;
+// var count = 0;
 
-function cc(card) {
-  // Only change code below this line
-switch (card) {
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-        count + 1;
-        break;
-    case 7:
-    case 8:
-    case 9:
-        count;
-        break;
-    case 10:
-    case "J":
-    case "Q":
-    case "K":
-    case "A":
-        count -1;
-        break;
-}
-if (count >=1){
-    return count + " Bet";
-} else {
-    return count + " Hold"
-}
-  // Only change code above this line
-}
+// function cc(card) {
+//   // Only change code below this line
+// switch (card) {
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//     case 6:
+//         count + 1;
+//         break;
+//     case 7:
+//     case 8:
+//     case 9:
+//         count;
+//         break;
+//     case 10:
+//     case "J":
+//     case "Q":
+//     case "K":
+//     case "A":
+//         count -1;
+//         break;
+// }
+// if (count >=1){
+//     return count + " Bet";
+// } else {
+//     return count + " Hold"
+// }
+//   // Only change code above this line
+// }
 
-cc(2); cc(3); cc(7); cc('K'); cc('A');
+// cc(2); cc(3); cc(7); cc('K'); cc('A');
 
+
+
+function checkObj(obj, checkProp) {
+    // Only change code below this line
+    if(obj.hasOwnProperty(checkProp)==true) {
+      return obj[checkProp];
+    } else {
+      return "Not Found";
+    }
+    // Only change code above this line
+  }
+
+  console.log(checkObj({gift: "pony", pet: "kitten", bed: "sleigh"}, "gift"))
