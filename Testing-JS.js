a15049b965580bde5b446b4a40bd3fd0375c6a5a
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-12-06 15:22:55 +0000

    Update Testing-JS.js

--- Testing-JS.js
+++ Testing-JS.js
@@ -76,52 +76,47 @@
 
 
 // Setup
-var collection = {
-  2548: {
-    albumTitle: 'Slippery When Wet',
-    artist: 'Bon Jovi',
-    tracks: ['Let It Rock', 'You Give Love a Bad Name']
-  },
-  2468: {
-    albumTitle: '1999',
-    artist: 'Prince',
-    tracks: ['1999', 'Little Red Corvette']
-  },
-  1245: {
-    artist: 'Robert Palmer',
-    tracks: []
-  },
-  5439: {
-    albumTitle: 'ABBA Gold'
-  }
-};
+// var collection = {
+//   2548: {
+//     albumTitle: 'Slippery When Wet',
+//     artist: 'Bon Jovi',
+//     tracks: ['Let It Rock', 'You Give Love a Bad Name']
+//   },
+//   2468: {
+//     albumTitle: '1999',
+//     artist: 'Prince',
+//     tracks: ['1999', 'Little Red Corvette']
+//   },
+//   1245: {
+//     artist: 'Robert Palmer',
+//     tracks: []
+//   },
+//   5439: {
+//     albumTitle: 'ABBA Gold'
+//   }
+// };
 
-// Only change code below this line
-function updateRecords(object, id, prop, value) {
-  if(prop !== "tracks" && value !== "") {
-    object[id][prop] = value;
-  } else if (prop === "tracks" && object[id].hasOwnProperty(prop)==false) {
-    object[id][prop] = [value];
-  } else if (prop === "tracks" && value !== "" ) {
-    object[id][prop].push(value);
-  } else if (value === "") {
-    delete object[id][prop];
-  }
-  return object;
-}
+// // Only change code below this line
+// function updateRecords(object, id, prop, value) {
+//   if(prop !== "tracks" && value !== "") {
+//     object[id][prop] = value;
+//   } else if (prop === "tracks" && object[id].hasOwnProperty(prop)==false) {
+//     object[id][prop] = [value];
+//   } else if (prop === "tracks" && value !== "" ) {
+//     object[id][prop].push(value);
+//   } else if (value === "") {
+//     delete object[id][prop];
+//   }
+//   return object;
+// }
 
-console.log(updateRecords(collection, 5439, "artist", "ABBA"));
-console.log(updateRecords(collection, 5439, "tracks", "Take a Chance on Me"));
-console.log(updateRecords(collection, 2548, "artist", ""));
-console.log(updateRecords(collection, 1245, "tracks", "Addicted to Love"));
-console.log(updateRecords(collection, 2468, "tracks", "Free"));
-console.log(updateRecords(collection, 2548, "tracks", ""));
-console.log(updateRecords(collection, 1245, "albumTitle", "Riptide"))
-
-
-
-
-
+// console.log(updateRecords(collection, 5439, "artist", "ABBA"));
+// console.log(updateRecords(collection, 5439, "tracks", "Take a Chance on Me"));
+// console.log(updateRecords(collection, 2548, "artist", ""));
+// console.log(updateRecords(collection, 1245, "tracks", "Addicted to Love"));
+// console.log(updateRecords(collection, 2468, "tracks", "Free"));
+// console.log(updateRecords(collection, 2548, "tracks", ""));
+// console.log(updateRecords(collection, 1245, "albumTitle", "Riptide"))
 
 /*
 Your function must always return the entire object.
@@ -136,3 +131,16 @@ If value is an empty string, delete the given prop property from the album.
 
 Note: A copy of the collection object is used for the tests.
 */
+
+
+// Setup
+var myArray = [];
+
+// Only change code below this line
+var i = 5;
+while (i>0) {
+    myArray.push(i);
+    i++;
+}
+
+console.log(myArray)
