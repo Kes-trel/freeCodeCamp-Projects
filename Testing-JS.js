d43a0ae434a15c7e3d718995cbf6336c2e509fae
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-12-07 19:55:58 +0000

    Update Testing-JS.js

--- Testing-JS.js
+++ Testing-JS.js
@@ -149,51 +149,59 @@ Note: A copy of the collection object is used for the tests.
 //   console.log(multiplyAll([[1,2],[3,4],[5,6,7]]));
 
   
-// Setup
-var contacts = [
-  {
-      "firstName": "Akira",
-      "lastName": "Laine",
-      "number": "0543236543",
-      "likes": ["Pizza", "Coding", "Brownie Points"]
-  },
-  {
-      "firstName": "Harry",
-      "lastName": "Potter",
-      "number": "0994372684",
-      "likes": ["Hogwarts", "Magic", "Hagrid"]
-  },
-  {
-      "firstName": "Sherlock",
-      "lastName": "Holmes",
-      "number": "0487345643",
-      "likes": ["Intriguing Cases", "Violin"]
-  },
-  {
-      "firstName": "Kristian",
-      "lastName": "Vos",
-      "number": "unknown",
-      "likes": ["JavaScript", "Gaming", "Foxes"]
-  }
-];
+// // Setup
+// var contacts = [
+//   {
+//       "firstName": "Akira",
+//       "lastName": "Laine",
+//       "number": "0543236543",
+//       "likes": ["Pizza", "Coding", "Brownie Points"]
+//   },
+//   {
+//       "firstName": "Harry",
+//       "lastName": "Potter",
+//       "number": "0994372684",
+//       "likes": ["Hogwarts", "Magic", "Hagrid"]
+//   },
+//   {
+//       "firstName": "Sherlock",
+//       "lastName": "Holmes",
+//       "number": "0487345643",
+//       "likes": ["Intriguing Cases", "Violin"]
+//   },
+//   {
+//       "firstName": "Kristian",
+//       "lastName": "Vos",
+//       "number": "unknown",
+//       "likes": ["JavaScript", "Gaming", "Foxes"]
+//   }
+// ];
 
 
-function lookUpProfile(name, prop){
-// Only change code below this line
-for (var listIndex = 0; listIndex < contacts.length; listIndex++) {
-  if (name === contacts[listIndex].firstName) {
-    if (contacts[listIndex].hasOwnProperty(prop)===true) {
-      return contacts[listIndex][prop];
-  } else {
-      return "No such property";
-    }
-  }
+// function lookUpProfile(name, prop){
+// // Only change code below this line
+// for (var listIndex = 0; listIndex < contacts.length; listIndex++) {
+//   if (name === contacts[listIndex].firstName) {
+//     if (contacts[listIndex].hasOwnProperty(prop)===true) {
+//       return contacts[listIndex][prop];
+//   } else {
+//       return "No such property";
+//     }
+//   }
+// }
+// return "No such contact";
+// // Only change code above this line
+// }
+
+// console.log(lookUpProfile("Harry", "likes"));
+
+
+function randomFraction() {
+
+  // Only change code below this line
+
+  return Math.random(0);
+
+  // Only change code above this line
 }
-return "No such contact";
-// Only change code above this line
-}
-
-console.log(lookUpProfile("Harry", "likes"));
-
-
-
+ console.log(randomFraction())
