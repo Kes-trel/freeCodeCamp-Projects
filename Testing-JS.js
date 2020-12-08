8039bc9052835c109a08c23149796d15f05c638f
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-12-08 16:15:16 +0000

    Update Testing-JS.js

--- Testing-JS.js
+++ Testing-JS.js
@@ -229,11 +229,11 @@ Note: A copy of the collection object is used for the tests.
 
 
 
-function convertToInteger(str) {
-return parseInt(str, 2); //The parseInt() function parses a string and returns an integer. It takes a second argument for the radix, which specifies the base of the number in the string. The radix can be an integer between 2 and 36.
-}
+// function convertToInteger(str) {
+// return parseInt(str, 2); //The parseInt() function parses a string and returns an integer. It takes a second argument for the radix, which specifies the base of the number in the string. The radix can be an integer between 2 and 36.
+// }
 
-console.log(convertToInteger("10011"));
+// console.log(convertToInteger("10011"));
 
 
 
