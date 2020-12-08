9912904b2f803fd32d2b352810c300255afd4728
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-12-08 15:43:01 +0000

    Update Testing-JS.js

--- Testing-JS.js
+++ Testing-JS.js
@@ -206,4 +206,11 @@ Note: A copy of the collection object is used for the tests.
 // }
 //  console.log(randomFraction())
 
+function randomRange(myMin, myMax) {
+  // Only change code below this line
+  return Math.floor(Math.random()*(myMax-myMin+1)+myMin);
+  ;
+  // Only change code above this line
+}
 
+console.log(randomRange(10, 20))
