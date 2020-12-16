87353072cdf53a06400c3a466da94b1cc1737cda
Kes <74243456+Kes-trel@users.noreply.github.com>
2020-12-16 10:19:37 +0000

    Update Testing-JS.js

--- Testing-JS.js
+++ Testing-JS.js
@@ -375,7 +375,7 @@ class Thermostat {
 
 const thermos = new Thermostat(76); // Setting in Fahrenheit scale
 let temp = thermos.temperature; // 24.44 in Celsius
-thermos.temperature = 30;
+thermos.temperature = 26;
 temp = thermos.temperature; // 26 in Celsius
 
-console.log(thermos._tempC)
+console.log(temp)
